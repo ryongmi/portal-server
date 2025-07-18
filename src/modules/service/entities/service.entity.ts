bbc3389a69b1dddd60e009609fcef6d398a8343f
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-18 16:14:28 +0900

    feat(service) - nullable 필드 null 타입 추가

--- src/modules/service/entities/service.entity.ts
+++ src/modules/service/entities/service.entity.ts
@@ -8,10 +8,10 @@ export class ServiceEntity extends BaseEntityUUID {
   name!: string;
 
   @Column({ type: 'varchar', length: 255, nullable: true })
-  description?: string; // 홈페이지 가입 이메일, 통합 기준
+  description?: string | null; // 홈페이지 가입 이메일, 통합 기준
 
   @Column({ type: 'varchar', length: 2048, nullable: true })
-  baseUrl?: string; // 실제 서비스 URL
+  baseUrl?: string | null; // 실제 서비스 URL
 
   @Column({ type: 'boolean', default: true })
   isVisible?: boolean; // 포털에서 표시 여부
@@ -20,8 +20,9 @@ export class ServiceEntity extends BaseEntityUUID {
   isVisibleByRole?: boolean; // 권한 기반 표시 여부
 
   @Column({ type: 'varchar', length: 50, nullable: true })
-  displayName?: string; // 사용자에게 보여줄 이름 (예: "관리자 포털")
+  displayName?: string | null; // 사용자에게 보여줄 이름 (예: "관리자 포털")
 
   @Column({ type: 'varchar', length: 2048, nullable: true })
-  iconUrl?: string; // UI에 표시할 서비스 아이콘
+  iconUrl?: string | null; // UI에 표시할 서비스 아이콘
 }
+
